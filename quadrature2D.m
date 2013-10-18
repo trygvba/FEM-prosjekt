6df3a4bb51750fba93f07ba3001d56bcf8d31a23
Trygve Bærland <trygvba@baktus.math.ntnu.no>
2013-10-18 12:55:36 +0200

    Fil for oppgave 1b) part 1.

---
+++ quadrature2D.m
@@ -0,0 +1,25 @@
+function I = quadrature2D(p1, p2, p3, Nq, g)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+switch Nq
+    case 1
+        eta = 1/3*[1 1 1];
+        weights = 1;
+    case 3
+        eta = 0.5*[1 1 0; 1 0 1; 0 1 1];
+        weights = 1/3*[1 1 1];
+    case 4
+        eta = 1/15*[5 5 5; 9 3 3; 3 9 3; 3 3 9];
+        weights = 1/48*[-27 25 25 25];
+end
+
+I=0;
+for i=1:Nq
+    x = p1*eta(i,1)+p2*eta(i,2)+p3*eta(i,3);
+    I = I + weights(i)*g(x(1),x(2));
+end
+
+end
+
